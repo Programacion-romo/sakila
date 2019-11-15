3ab69d35dfcc92d2315e8c498bd3c25d437312f4
juanca035 <56207190+juanca035@users.noreply.github.com>
2019-11-14 19:19:21 -0500

    Add files via upload

---
+++ Modelo-camilo/modeloconsulta.php
@@ -0,0 +1,102 @@
+<?php
+    require_once("modeloAbstractoDB.php");
+	
+    class consulta extends ModeloAbstractoDB {
+		private $film_id;
+		private $category_id;
+		private $store_id;
+		
+		function __construct() {
+			//$this->db_category_id = '';
+		}
+
+		public function getFilm_id(){
+			return $this->film_id;
+		}
+
+		public function getCategory_id(){
+			return $this->category_id;
+		}
+		
+		public function getStore_id(){
+			return $this->store_id;
+		}
+
+		public function consultar($film_id='') {
+			if($film_id !=''):
+				$this->query = "
+				SELECT film_id, category_id, store_id
+				FROM query
+				WHERE film_id = '$film_id' order by film_id
+				";
+				$this->obtener_resultados_query();
+			endif;
+			if(count($this->rows) == 1):
+				foreach ($this->rows[0] as $propiedad=>$valor):
+					$this->$propiedad = $valor;
+				endforeach;
+			endif;
+		}
+		
+		public function lista() {
+			$this->query = "
+			SELECT m.film_id, y.title, x.name, i.store_id
+			FROM query as m
+			inner join film as y on (m.film_id=y.film_id) 
+            inner join category as x on (m.category_id=x.category_id)
+            inner join inventory as i on (m.film_id=i.film_id)
+                 order by title
+			";
+			
+			$this->obtener_resultados_query();
+			return $this->rows;
+			
+		}
+		public function nuevo($datos=array()) {
+			if(array_key_exists('film_id', $datos)):
+				foreach ($datos as $campo=>$valor):
+					$$campo = $valor;
+				endforeach;
+				$category_id= utf8_decode($category_id);
+				$this->query = "
+					INSERT INTO query
+					(film_id, category_id, store_id)
+					VALUES
+					('$film_id', '$category_id',$store_id)
+					";
+				$resultado = $this->ejecutar_query_simple();
+				return $resultado;
+			endif;
+		}
+		
+		public function editar($datos=array()) {
+			foreach ($datos as $campo=>$valor):
+				$$campo = $valor;
+			endforeach;
+			$category_id= utf8_decode($category_id);
+			$this->query = "
+			UPDATE query
+			SET film_id='$film_id',
+			category_id='$category_id'
+			store_id='$store_id'
+			WHERE film_id = '$film_id'
+			";
+			$resultado = $this->ejecutar_query_simple();
+			return $resultado;
+		}
+		
+		public function borrar($film_id='') {
+			$this->query = "
+			DELETE FROM query 
+			WHERE film_id = '$film_id'
+			";
+			$resultado = $this->ejecutar_query_simple();
+
+			return $resultado;
+		}
+		
+		function __destruct() {
+			//unset($this);
+		}
+	}
+    ?>
