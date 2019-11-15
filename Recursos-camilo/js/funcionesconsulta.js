011ed6c8d5edd943ebae0776faf711dbfec9cc96
juanca035 <56207190+juanca035@users.noreply.github.com>
2019-11-14 19:20:02 -0500

    Add files via upload

---
+++ Recursos-camilo/js/funcionesconsulta.js
@@ -0,0 +1,31 @@
+function consulta(){
+
+    var dt = $("#tabla").DataTable({
+            "ajax": "./Controlador/controladorconsulta.php?accion=listar",
+            "columns": [
+              { "data": "film_id"} ,
+              { "data": "title"} ,
+              { "data": "name"} ,
+              { "data": "store_id"}, 
+              { "data": "film_id",
+              render: function (data) {
+                        return '<a href="#" data-codigo="'+ data + 
+                               '" class="btn btn-danger btn-sm borrar"> <i class="fa fa-trash"></i></a>'
+                        +      '<a href="#" data-codigo="'+ data + 
+                               '" class="btn btn-info btn-sm editar"> <i class="fa fa-edit"></i></a>'
+                              }
+                            } 
+              
+            ]
+    });
+    $("#editar").on("click",".btncerrar", function(){
+        $(".box-title").html("Listado de peliculas categorias");
+        $("#editar").addClass('hide');
+        $("#editar").removeClass('show');
+        $("#listado").addClass('show');
+        $("#listado").removeClass('hide');  
+        $(".box #nuevo").show();  
+  
+      }) 
+
+}
